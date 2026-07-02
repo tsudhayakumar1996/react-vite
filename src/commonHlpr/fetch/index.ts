ac61b84d348cb077d6f44a205b888d507ac2dcb9
Udhay <udhayakumar.t@in.flowglobal.net>
2026-07-02 08:37:55 +0530

    feat(auth): google oauth setup

--- src/commonHlpr/fetch/index.ts
+++ src/commonHlpr/fetch/index.ts
@@ -2,6 +2,8 @@ export const connectServerGet = (endPoint: string) =>
   fetch(import.meta.env.VITE_SERVER_END_POINT + endPoint).then((r) => r.json())
 
 export const connectServerPost = <T>(endPoint: string, payload: T) =>
-  fetch(import.meta.env.VITE_SERVER_END_POINT + endPoint, { method: 'POST', body: JSON.stringify(payload) }).then((r) =>
-    r.json()
-  )
+  fetch(import.meta.env.VITE_SERVER_END_POINT + endPoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  }).then((r) => r.json())
